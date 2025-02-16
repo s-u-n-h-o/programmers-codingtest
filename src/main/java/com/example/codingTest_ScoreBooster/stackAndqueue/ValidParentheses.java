11df2f7fdd6f1407ded42ffd662ab6e59d29ff34
s-u-n-h-o <chltjsgh06@gmail.com>
2025-02-17 01:09:31 +0900

    [QUEUE] 올바른 괄호(Lv.2) - 각 괄호마다 변수의 증감으로 해결

--- src/main/java/com/example/codingTest_ScoreBooster/stackAndqueue/ValidParentheses.java
+++ src/main/java/com/example/codingTest_ScoreBooster/stackAndqueue/ValidParentheses.java
@@ -5,15 +5,13 @@ import java.util.Queue;
 
 //올바른 괄호
 public class ValidParentheses {
-
-    /**
-     *        s	    answer
-     *     "()()"	true
-     *     "(())()"	true
-     *     ")()("	false
-     *     "(()("	false
+    /***
+     * [문제 틀림]
+     * 1. 일단 문제를 너무 꼬아서 생각했다.
+     * 2. (()) <- 이 부분을 푼다고 생각하면서 앞과 뒤의 값이 동일한지 동일하지 않은지 2분법으로 생각해버렸다
+     * @param s
+     * @return
      */
-
     boolean solution(String s) {
         boolean answer = true;
         int balance = 1;
@@ -30,7 +28,8 @@ public class ValidParentheses {
         }
 
         while (!parentheses.isEmpty()) {
-            System.out.println("parentheses.peek() : " + parentheses.peek());
+            System.out.println("chkParentheses : " + chkParentheses);
+            System.out.println("parentheses.peek : " + parentheses.peek());
             if(chkParentheses.equals(parentheses.peek())) { //앞의 값이랑 뒤의값이 동일한경우
                 balance++;
                 chkParentheses = parentheses.poll();
@@ -51,13 +50,77 @@ public class ValidParentheses {
         return answer;
     }
 
+    /**
+     * 다음 기호랑 비교하는것이 아니라 카운트값을 통해서 값이 일치하는지 확인하는방법으로 문제풀이
+     * @param s
+     * @return
+     */
+    boolean solution1(String s) {
+        int count = 0;
+
+        Queue<String> parentheses = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            parentheses.add(String.valueOf(s.charAt(i)));
+        }
+
+        if(parentheses.peek().equals(")")) {
+            return false;
+        }
+
+        //stack에서 하나씩 꺼내서 확인하기
+        for (int i = 0; i < s.length(); i++) {
+            if(parentheses.peek().equals("(")) {
+                count++;
+            } else if (parentheses.peek().equals(")")) {
+                count--;
+            }
+            parentheses.remove();
+
+            if(count < 0) { //음수일경우 "())(()"
+                return false;
+            }
+        }
+        return count == 0 ? true : false;
+    }
+
+    /***
+     *  solution1 보다 좀더 정리해서 간결하게
+     * @param s
+     * @return
+     */
+    boolean solution2(String s) {
+        int count = 0;
+        boolean answer = true;
+
+        Queue<String> parentheses = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            parentheses.add(String.valueOf(s.charAt(i)));
+        }
+
+        //stack에서 하나씩 꺼내서 확인하기
+        for (int i = 0; i < s.length(); i++) {
+            if(parentheses.peek().equals("(")) {
+                count++;
+            }
+            if(parentheses.peek().equals(")")) {
+                count--;
+            }
+            if(count < 0) {
+                answer = false;
+                break;
+            }
+            parentheses.remove();
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         ValidParentheses validParentheses = new ValidParentheses();
-        //validParentheses.solution("()()"); //true
-        //validParentheses.solution("(())()");//true
-        //validParentheses.solution(")()(");//false
-        //validParentheses.solution("(()(");//false
-        validParentheses.solution("())(()");//false
+        //validParentheses.solution1("()()"); //true
+        //validParentheses.solution1("(())()");//true
+        //validParentheses.solution1(")()(");//false
+        //validParentheses.solution1("(()(");//false
+        validParentheses.solution1("())(()");//false
     }
 
 }
