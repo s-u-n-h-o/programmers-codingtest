6dca57629e8eceea99d6945a359d01e783b10ed7
s-u-n-h-o <chltjsgh06@gmail.com>
2025-02-19 18:21:13 +0900

    <SORTING> k번째수(Lv.1) - 정렬 copyOfRange()사용

---
+++ src/main/java/com/example/codingTest_ScoreBooster/sorting/KthNumber.java
@@ -0,0 +1,28 @@
+package com.example.codingTest_ScoreBooster.sorting;
+
+import java.util.Arrays;
+
+//k번쨰 수
+public class KthNumber {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < commands.length; i++) {
+            start = commands[i][0]-1;
+            end = commands[i][1];
+
+            int[] arr = Arrays.stream(Arrays.copyOfRange(array,start,end)).sorted().toArray();
+            int tmp = arr[commands[i][2]-1];
+            answer[i] = tmp;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        KthNumber kthNumber = new KthNumber();
+        kthNumber.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}); //[5, 6, 3]
+    }
+}
