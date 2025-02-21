bfc8a243b7e6cd67db0e7fdc9dacff152c60cdfd
s-u-n-h-o <chltjsgh06@gmail.com>
2025-02-21 17:59:19 +0900

    [HASH] 13414 수강신청 : LinkedHashSet을 사용하여 풀이 key값만 있으면 된다고 판단하여 순서는 보장하되 중복을 허용하지않는 linkedHashSet을 사용했는데 HashMap으로도 풀이가 가능함을 확인

---
+++ src/main/java/com/example/baekjoon/courseRegistration13414.java
@@ -0,0 +1,38 @@
+package com.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+//수강신청
+public class courseRegistration13414 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int k = Integer.parseInt(st.nextToken());
+        int len = Integer.parseInt(st.nextToken());
+
+        LinkedHashSet<String> hashSet = new LinkedHashSet<>();
+
+        for(int i = 0 ; i < len ; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (hashSet.contains(str)) { //두번 중복을 눌렀을경우
+                hashSet.remove(str);
+            }
+            hashSet.add(str);
+        }
+
+        Iterator iter = hashSet.iterator();	// Iterator 사용
+        int count = 0;
+        while (iter.hasNext() && count < k) {
+            System.out.println(iter.next());
+            count++;
+        }
+    }
+}
---
+++ src/main/java/com/example/codingTest_scoreBooster/sorting/TheLargestNumber.java
@@ -0,0 +1,4 @@
+package com.example.codingTest_scoreBooster.sorting;
+
+public class TheLargestNumber {
+}
