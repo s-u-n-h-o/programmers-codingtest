6e3b9abc3e2371e8b6ead2732585b4a3e45e2685
s-u-n-h-o <chltjsgh06@gmail.com>
2025-02-24 00:21:05 +0900

    [sort] 가장큰수 (Lv.2) Integer.compare()를 활용하여 비교후 내림차순으로 정렬하는법 배움

--- src/main/java/com/example/codingTest_scoreBooster/sorting/LargestNumber.java
+++ src/main/java/com/example/codingTest_scoreBooster/sorting/LargestNumber.java
@@ -1,6 +1,6 @@
 package com.example.codingTest_scoreBooster.sorting;
 
-import java.util.Arrays;
+import java.util.*;
 
 //가장 큰수
 public class LargestNumber {
@@ -50,6 +50,37 @@ public class LargestNumber {
             answer += number;
         }
         return answer;
+    }
+
+    /**
+     * 다른 사람의 풀이
+     * @param numbers
+     * @return
+     */
+    public String solution2(int[] numbers) {
+        String answer = "";
+
+        List<Integer> list = new ArrayList<>(); //arrayList 객체생성
+        for (int i = 0; i < numbers.length; i++) { //numbers배열만큼 반복하면서 list에 값 할당
+            list.add(numbers[i]);
+        }
+
+        Collections.sort(list, (a,b) ->{ //list에서 두개의 값을 꺼낸다
+            String as = String.valueOf(a), bs = String.valueOf(b); //각각 스트링 자료형을 만든다
+            //compare메소드 활용하는데 음수는 오름차순 양수는 내림차순으로 요소의 자리를 바꾼다
+            //즉 오름차순이지만 내림차순으로 만들어주기위해서 해당 결과에 '-'를 붙여주는것
+            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
+        });
+        StringBuilder sb = new StringBuilder();
+        for (Integer i : list) {
+            sb.append(i);
+        }
+        answer = sb.toString();
+        if (answer.charAt(0) == '0') {
+            return "0";
+        }else {
+            return answer;
+        }
     }
 
     public static void main(String[] args) {
