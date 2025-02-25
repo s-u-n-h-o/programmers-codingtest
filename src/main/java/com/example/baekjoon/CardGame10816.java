1104da8841207c1499e2663283e378055062ae15
s-u-n-h-o <chltjsgh06@gmail.com>
2025-02-25 17:41:36 +0900

    [HASH] 10816 카드게임2
    1. HashMap을 사용해서 풀이
     <시간초과가 발생>
    - HashMap<String,Integer> 형식으로 받아서 불필요한 문자변환 비용발생
    - System.out.print()를 여러번 호출하는것보다 StringBuilder를 사용해서 한번에 출력필요

--- src/main/java/com/example/baekjoon/CardGame10816.java
+++ src/main/java/com/example/baekjoon/CardGame10816.java
@@ -1,4 +1,37 @@
 package com.example.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+//카드게임2
 public class CardGame10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+
+        int number = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //카드의 개수만큼 반복
+        for (int i = 0; i < number; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            if(hashMap.containsKey(key)) {
+                hashMap.put(key, hashMap.get(key) + 1);
+            }else {
+                hashMap.put(key, 1);
+            }
+        }
+
+        int numberInteger = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < numberInteger; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(hashMap.getOrDefault(key, 0) + " ");
+        }
+        System.out.print(sb.toString());
+    }
 }
